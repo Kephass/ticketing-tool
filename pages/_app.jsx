55d00d2799de2911b72937293da4b53048c4d421
Kephass <odurof99@gmail.com>
2022-06-16 23:20:34 +0200

    basic styling
    1. Created the Header with a single route
    2. Added light and dark mode
    3. Added footer

---
+++ pages/_app.jsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<ChakraProvider>
+			<Header />
+			<Component {...pageProps} />
+			<Footer />
+		</ChakraProvider>
+	);
+}
+
+export default MyApp;
